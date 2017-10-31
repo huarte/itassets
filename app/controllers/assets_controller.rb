b7e91702023eeb8eaf94c3bd2a43e7d1735cbe56
Oriol Huarte <ohuarte@origbuff.local>
2017-10-31 16:41:46 +0100

    mod.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 class AssetsController < ApplicationController
-=======
-class ArticlesController < ApplicationController
->>>>>>> 03089d0bc95f734dc8005fcaec0010854aae17e9
   def new
   end
 end
